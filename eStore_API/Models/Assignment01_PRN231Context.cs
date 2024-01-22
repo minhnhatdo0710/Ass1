d727fdc1d6e049e3774265a4027f127b6a271e2c
phuongndHE153051 <105622118+phuongndHE153051@users.noreply.github.com>
2024-01-22 08:01:22 +0700

    context

--- eStore_API/Models/Assignment01_PRN231Context.cs
+++ eStore_API/Models/Assignment01_PRN231Context.cs
@@ -26,8 +26,8 @@ namespace eStore_API.Modelss
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost;uid=sa;pwd=Nghiaanh01@;Database=eStore");
+                var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseSqlServer(config.GetConnectionString("eStoreDB"));
             }
         }
 
